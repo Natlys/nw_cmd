dfcace2fb8dc3f3def428246d5aeb8ef596ce536
nikodim <nikodimcheer@gmail.com>
2021-03-28 21:04:56 +0800

    --==continuation==-- -> native_library causes new changes; -> names are altered; -> interaction with the library changes as well; -> reduntant(old) files are deleted; -> precompiled header was changed; -> premake file is altered; --==/continuation==--

--- src_cpp/native_console.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NATIVE_CONSOLE_HPP
-#define NATIVE_CONSOLE_HPP
-
-#include <nwc_engine.h>
-#include <nwc_engine_states.h>
-#include <nwc_framebuf.h>
-#include <nwc_gui.h>
-
-#endif	// NATIVE_CONSOLE_HPP
